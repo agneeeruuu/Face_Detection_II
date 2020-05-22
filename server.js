8a2dd4aa492ed4e938c6a8270852d0c2eaf0d159
p5l2b <agneeeruuu@gmail.com>
2020-05-23 02:25:05 +0800

    adding port

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
 app.post('/imageurl', (req, res) => { image.handleAPI(req, res) });
 
-app.listen(3000, () => {
-	console.log('app runs');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app runs on port ${process.env.PORT}`);
 })
 
 // /signin --> POST = success/fail (POST instead of query string!)
